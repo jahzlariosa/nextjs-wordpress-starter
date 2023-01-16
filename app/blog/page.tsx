6fd70dcc7606b9e07a22bfe84cbb02803e7d186d
Jahz Lariosa <dev@josephlariosa.com>
2023-01-17 07:46:37 +0800

    Pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
 
 interface Post {
   id: number;
@@ -12,19 +12,19 @@ interface Post {
   pageNumber: number;
 }
 
-
-const Blog = ({pageProps={}}) => {
-    const [pageNumber, setPageNumber] = useState(pageProps.pageNumber ? pageProps.pageNumber : 1);
-    const [posts, setPosts] = useState<Post[]>([]);
-    const [currentPage, setCurrentPage] = useState<number>(pageNumber);
-    const [totalPages, setTotalPages] = useState<number>(0);
-
+const Blog = ({ pageProps = { pageNumber: 1 } }) => {
+  const [pageNumber, setPageNumber] = useState(pageProps.pageNumber);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(pageNumber);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
-    fetch(`https://wp.jahz.xyz/wp-json/wp/v2/posts?page=${pageNumber}&per_page=2`)
-    .then((response) => {
+    fetch(
+      `https://wp.jahz.xyz/wp-json/wp/v2/posts?page=${pageNumber}&per_page=2`
+    )
+      .then((response) => {
         const totalPagesHeader = response.headers.get("x-wp-totalpages");
-        if(totalPagesHeader) {
+        if (totalPagesHeader) {
           setTotalPages(+totalPagesHeader);
         }
         return response.json();
@@ -46,15 +46,34 @@ const Blog = ({pageProps={}}) => {
           <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
         </div>
       ))}
-      <button disabled={currentPage === 1} onClick={() => setPageNumber(pageNumber - 1)}>Previous</button>
-      <div>
-  {Array.from({length: totalPages}, (_, i) => (
-    <button key={i + 1} onClick={() => setPageNumber(i + 1)}>{i + 1}</button>
-  ))}
+      <div className="flex justify-between">
+  <button
+    className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-medium py-2 px-4 rounded-l ${currentPage === 1 ? 'cursor-not-allowed opacity-50' : 'hover:bg-gray-500'}`}
+    disabled={currentPage === 1}
+    onClick={() => setPageNumber(pageNumber - 1)}
+  >
+    Previous
+  </button>
+  <div className="flex">
+    {Array.from({ length: totalPages }, (_, i) => (
+      <button
+        key={i + 1}
+        className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-medium py-2 px-4 rounded ${currentPage === i + 1 ? 'bg-gray-500' : 'hover:bg-gray-500'}`}
+        onClick={() => setPageNumber(i + 1)}
+      >
+        {i + 1}
+      </button>
+    ))}
+  </div>
+  <button
+    className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-medium py-2 px-4 rounded-r ${currentPage === totalPages ? 'cursor-not-allowed opacity-50' : 'hover:bg-gray-500'}`}
+    disabled={currentPage === totalPages}
+    onClick={() => setPageNumber(pageNumber + 1)}
+  >
+    Next
+  </button>
 </div>
-<button disabled={currentPage === totalPages} onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
-
-</div>
+    </div>
   );
 };
 
